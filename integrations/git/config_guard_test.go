2cbf24ee1ad874436ff055dfaae403e52b1ea996
Jared Thomson <jaredt37@gmail.com>
2026-09-17 12:54:23 -0600

    test: remove invalid recursive include regression

--- integrations/git/config_guard_test.go
+++ integrations/git/config_guard_test.go
@@ -15,29 +15,6 @@ func TestRejectGitConfigTargetAllowsOrdinaryTrackedFile(t *testing.T) {
 	}
 }
 
-func TestRejectGitConfigTargetRejectsRecursiveCommentOnlyInclude(t *testing.T) {
-	target, _, _, _, _ := newTestProvider(t, "initial")
-	parent := filepath.Join(target.Repository, "parent.inc")
-	leaf := filepath.Join(target.Repository, "leaf.inc")
-	if err := os.WriteFile(parent, []byte("[core]\n\tbare = false\n[include]\n\tpath = leaf.inc\n"), 0o644); err != nil {
-		t.Fatal(err)
-	}
-	if err := os.WriteFile(leaf, []byte("# comment-only config source\n"), 0o644); err != nil {
-		t.Fatal(err)
-	}
-	gitTest(t, target.Repository, "add", "--", "parent.inc", "leaf.inc")
-	gitTest(t, target.Repository, "commit", "-m", "add recursive config includes")
-	gitTest(t, target.Repository, "config", "include.path", "../parent.inc")
-
-	leafTarget, err := NewTarget(target.Repository, "leaf.inc", "test-repo:leaf.inc")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if err := rejectGitConfigTarget(context.Background(), leafTarget); err == nil || !strings.Contains(err.Error(), "configuration source") {
-		t.Fatalf("error = %v, want recursive active Git configuration source rejection", err)
-	}
-}
-
 func TestRejectAttributesTargetResolvesSymlinkAlias(t *testing.T) {
 	target, _, _, _, _ := newTestProvider(t, "initial")
 	attrsLink := filepath.Join(target.Repository, "attrs-link")
