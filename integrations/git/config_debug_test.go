b9baeaf520900a844b0cca91604dbe93bd3e5ca7
Jared Thomson <jaredt37@gmail.com>
2026-09-17 11:44:35 -0600

    test: format temporary Git config diagnostics

--- integrations/git/config_debug_test.go
+++ integrations/git/config_debug_test.go
@@ -11,7 +11,9 @@ func TestDebugGitConfigInclude(t *testing.T) {
 	target, _, _, _, _ := newTestProvider(t, "initial")
 	configPath := filepath.Join(target.Repository, ".git", "config")
 	includePath := filepath.Join(target.Repository, "tracked-config.inc")
-	if err := os.WriteFile(includePath, []byte("[core]\n\tattributesFile = /tmp/unused\n"), 0o644); err != nil { t.Fatal(err) }
+	if err := os.WriteFile(includePath, []byte("[core]\n\tattributesFile = /tmp/unused\n"), 0o644); err != nil {
+		t.Fatal(err)
+	}
 	gitTest(t, target.Repository, "add", "--", "tracked-config.inc")
 	gitTest(t, target.Repository, "commit", "-m", "add tracked config include")
 	gitTest(t, target.Repository, "config", "include.path", "../tracked-config.inc")
