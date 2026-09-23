6e2f2247eb00d62958b4301f7c8f1242d8b98bdb
Jared Thomson <jaredt37@gmail.com>
2026-09-22 21:42:52 -0600

    fix: restore known-good provider formatting

--- integrations/git/provider.go
+++ integrations/git/provider.go
@@ -3074,7 +3074,7 @@ func sanitizedGitEnv() []string {
 		"GIT_CEILING_DIRECTORIES":          {},
 		"GIT_DISCOVERY_ACROSS_FILESYSTEM":  {},
 		"GIT_GRAFT_FILE":                   {},
-		"GIT_ATTR_SOURCE":                  {}, // Reject environment-selected Git attribute sources.
+		"GIT_ATTR_SOURCE":                  {},
 	}
 	env := make([]string, 0, len(os.Environ()))
 	for _, entry := range os.Environ() {
@@ -3089,7 +3089,6 @@ func sanitizedGitEnv() []string {
 	}
 	return env
 }
-
 
 func rejectCommandScopeConfigEnvironment() error {
 	for _, entry := range os.Environ() {
